bdbb570ef13225ffc4f308ea30651f7aa3378e25
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-11-12 10:31:06 +0200

    webcam

--- galary.php
+++ galary.php
@@ -10,6 +10,7 @@ $i = 0;
 
 $user = new User();
 $nextpg = $page_num+1;
+$prevpg = $page_num-1;
 if ($user->isLoggedIn()){
        // $id = $user->data()->id;
         echo "<h1 style= 'text-align: center; padding-bottom: 30px;'>GALARY</h1>";
@@ -20,23 +21,26 @@ if ($user->isLoggedIn()){
                 <form class='del' method='post'>
                     <label for='name'>All Images</label><br>
                 </form>
-                <form method='post'>
-                    <button name='count'>next</button>
-                    <button name='prev'>prev</button>
-                </form>
-                <a href='galary.php?pagenum=$nextpg'a/>
+                
             </body>
         </html>
         ";
     try {
+        $amount = 6;
+        $start = ($page_num - 1) * $amount ;
 		$conn = new PDO("mysql:host=localhost;dbname=camagru", "root", "123456");
 		// if ($conn) echo "Connection successful <br/>";
-            $query = "SELECT * FROM camagru.images";
+            $query = "SELECT * FROM camagru.images LIMIT  $start , $amount";
             $stmt = $conn->prepare($query);
             $stmt->execute();
             $count = $stmt->rowCount();
             $likes = 0;
-            echo "num image = " . $count . "<br/>";
+            
+            if ($page_num >= 2)
+                echo "<a href='galary.php?pagenum=$prevpg'><-PREV<a/><br/>";
+            if ($count >= $amount)
+                echo "<a href='galary.php?pagenum=$nextpg'>NEXT |-><a/><br/>";
+
             $res = array_reverse($stmt->fetchAll());
             $i = 0;
             foreach ($res as $image) {
@@ -71,34 +75,55 @@ if ($user->isLoggedIn()){
 	echo "ERROR: " . $ex->getMessage() . "<br/>";
 }
 }else{
-    echo "
+           // $id = $user->data()->id;
+        echo "<h1 style= 'text-align: center; padding-bottom: 30px;'>GALARY</h1>";
+        echo "<a href='index.php'><button>HOME</button></a><br><br>";
+        echo "
         <html>
             <body style = 'background-color:gray; text-align:center;'>
                 <form class='del' method='post'>
-                    <label for='name'><h1>Galary</h1></label><br>
+                    <label for='name'>All Images</label><br>
                 </form>
+                
             </body>
         </html>
         ";
-    $conn = new PDO("mysql:host=localhost;dbname=camagru", "root", "123456");
-        // if ($conn) echo "Connection successful <br/>";
-    echo "<a href='index.php'><button>HOME</button></a><br><br>";
-            $query = "SELECT * FROM camagru.images limit 1";
+    try {
+        $amount = 6;
+        $start = ($page_num - 1) * $amount ;
+		$conn = new PDO("mysql:host=localhost;dbname=camagru", "root", "123456");
+            $query = "SELECT * FROM camagru.images LIMIT  $start , $amount";
             $stmt = $conn->prepare($query);
             $stmt->execute();
             $count = $stmt->rowCount();
-            //echo "num image = " . $count . "<br/>";
+            $likes = 0;
+            
+            if ($page_num >= 2)
+                echo "<a href='galary.php?pagenum=$prevpg'><-PREV<a/><br/>";
+            if ($count >= $amount)
+                echo "<a href='galary.php?pagenum=$nextpg'>NEXT |-><a/><br/>";
+
             $res = array_reverse($stmt->fetchAll());
-            // for (i from 1*page to 5 * page){
-            //     $image = $res[i]
-            // }
+            $i = 0;
             foreach ($res as $image) {
                 $ima = $image['image_name'];
                 echo "
                         <style type='text/css'> img{ height: 200px; width: 200px;}</style>
-                        <button type='submit'><img src='uploads/$ima' name='$ima' legth='=30%' width='30%' border='8px solid black'><h3>name $ima<h3></img></button></a>
+                            <button type='submit'>
+                               <img src='uploads/$ima' name='$ima' legth='=30%' width='30%' border='8px solid black'>
+                                </img>
+                        </button></a>      
                 ";
-        }
+                if($i == 2){
+                        echo '<br>';
+                        $i = -1;
+                    }
+                $i++;
+            }
+            $c = 0;
+}catch(PDOException $ex) {
+	echo "ERROR: " . $ex->getMessage() . "<br/>";
+}
 }
 ?>
 
